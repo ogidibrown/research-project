29e61f19cb5022440d63d08d7bc318945002a3e2
Gideon Anokye <gideonanokyebrown@gmail.com>
2023-07-06 15:48:01 +0000

    modified changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,10 +10,10 @@ const footer = () => {
     <footer className='h-[50px] text-center flex flex-col md:flex-row items-center md:justify-between gap-3'>
         <div className='text-[14px]'>@2023 TELEREHABILITATION. All rights reserved.</div>
         <div className='flex gap-[12px] items-center'>
-        <Image src='/img1.png' width={20} height={20}  alt='facebook' className=' cursor-pointer mt-3 gap-2' />
-        <Image src='/img2.png' width={20} height={20}  alt='instagram' className=' cursor-pointer mt-3 gap-2' />
-        <Image src='/img3.png' width={20} height={20}  alt='linkdin' className=' cursor-pointer mt-3 gap-2' />
-        <Image src='/img4.png' width={22} height={22}  alt='youtube' className=' cursor-pointer mt-3 gap-2' />
+        <Image src='/img1.png' width={20} height={20}  alt='facebook' className=' cursor-pointer mt-3 ml-2' />
+        <Image src='/img2.png' width={20} height={20}  alt='instagram' className=' cursor-pointer mt-3 ml-2' />
+        <Image src='/img3.png' width={20} height={20}  alt='linkdin' className=' cursor-pointer mt-3 ml-2' />
+        <Image src='/img4.png' width={22} height={22}  alt='youtube' className=' cursor-pointer mt-3 ml-2' />
         </div>
     </footer>
   )
