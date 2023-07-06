0fd5195455317c00e4492ddd762526e988f04a1b
Gideon Anokye <gideonanokyebrown@gmail.com>
2023-07-06 15:21:05 +0000

    sixth july

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,11 +9,11 @@ const footer = () => {
   return (
     <footer className='h-[50px] text-center flex flex-col md:flex-row items-center md:justify-between gap-3'>
         <div className='text-[14px]'>@2023 TELEREHABILITATION. All rights reserved.</div>
-        <div className='flex gap-[8px] items-center'>
-        <Image src='/img1.png' width={20} height={20} alt='facebook' className=' cursor-pointer' />
-        <Image src='/img2.png' width={20} height={20} alt='instagram' className=' cursor-pointer' />
-        <Image src='/img3.png' width={20} height={20} alt='linkdin' className=' cursor-pointer' />
-        <Image src='/img4.png' width={22} height={22} alt='youtube' className=' cursor-pointer' />
+        <div className='flex gap-[12px] items-center'>
+        <Image src='/img1.png' width={20} height={20}  alt='facebook' className=' cursor-pointer p-[12px]' />
+        <Image src='/img2.png' width={20} height={20}  alt='instagram' className=' cursor-pointer p-[12px]' />
+        <Image src='/img3.png' width={20} height={20}  alt='linkdin' className=' cursor-pointer p-[12px]' />
+        <Image src='/img4.png' width={22} height={22}  alt='youtube' className=' cursor-pointer p-[12px]' />
         </div>
     </footer>
   )
